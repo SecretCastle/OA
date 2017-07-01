b6defeffbf7306f3dd1658dde0606de17a9392f7
fansOnly <fansOnly@users.noreply.github.com>
2017-07-01 18:22:35 +0800

    Add files via upload

---
+++ src/src/components/mail/company.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <vlist :header="headerD" :list="listD" :type="typeD"></vlist>
+        <div class="space"></div>
+        <vlist :header="headerE" :list="listE" :type="typeE"></vlist>
+    </div>
+</template>
+<script>
+import vlist from '@/libs/list';
+export default {
+    name: 'Company',
+    components: { vlist },
+    data() {
+        return {
+            typeD: '1',
+            headerD: '部门',
+            listD: [],
+            typeE: '2',
+            headerE: '联系人',
+            listE: [],
+        }
+    },
+    created() {
+    },
+    mounted() {
+        this.$store.commit('rightTab',"编辑部门");
+        this.$store.commit('hasFoot',{hasFoot:false});
+
+        var that = this;
+        $ajax.get('/UserApi/getDepartList?parentDepartId=' + this.$route.params.pid)
+        .then(res=>{
+            console.log(res)
+            var Departs = res.data.Departs,
+                Employees = res.data.Employees;
+            for(var i in Departs){
+                Departs[i].url = './' + Departs[i].Id
+            }
+            that.listD = Departs;
+            for(var i in Employees){
+                Employees[i].src = Employees[i].Avatar,
+                Employees[i].url = '../emp/' + Employees[i].Id
+            }
+            that.listE = res.data.Employees;
+            var pTitle = res.data.Name ? res.data.Name : '公司'
+            this.$store.commit('getTitle',pTitle);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+    },
+    computed: {},
+    methods: {
+    },
+    destroyed() {}
+}
+</script>
+<style scoped>
+</style>
